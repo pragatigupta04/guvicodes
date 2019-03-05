37a2c6aa61bde14ea4da29eeb7d20f75808595d5
pragatigupta04 <42873896+pragatigupta04@users.noreply.github.com>
2019-03-05 19:53:16 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Main
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n==0)
+{
+System.out.println("Zero");
+}
+else if(n>0)
+{
+System.out.println("Positive");
+}
+else
+{
+System.out.println("Negative");
+}
+}
+}
